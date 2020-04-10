21e8fa0a7a0689abe4417d5e46d03a585c51a08a
Paulo Alves Pereira <paulo.alves@seera.sa>
2020-04-10 14:32:48 +0100

    Chapter10: Update package versions and Dockerfile
    * Update package versions and lock them
    * Use a python 3.8 docker base image
    * Address PEP8: E302 formatting warnings

--- chapter-10/market-data/tests/test_service.py
+++ chapter-10/market-data/tests/test_service.py
@@ -1,11 +1,13 @@
 import pytest
 from service import app
 
+
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
     client = app.test_client()
     return client
+
 
 def test_index(client):
     rv = client.get('/ping')
