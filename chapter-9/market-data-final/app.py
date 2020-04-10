09728659ed8962d4ac58055c898f86d312adf41d
Paulo Alves Pereira <paulo.alves@seera.sa>
2020-04-10 14:20:30 +0100

    Chapter9: Update package versions and Dockerfile
    * Update package versions and lock them
    * Use a python 3.8 docker base image
    * Address PEP8: E302 formatting warnings

--- chapter-9/market-data-final/app.py
+++ chapter-9/market-data-final/app.py
@@ -4,9 +4,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/ping', methods=["GET"])
 def ping():
     return socket.gethostname()
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
